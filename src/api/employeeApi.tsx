239343d4bfa058284c3b480f924bd3bb714c885a
unknown <sally-draycott@hotmail.co.uk>
2025-07-30 19:03:28 +0100

    added styling

--- src/api/employeeApi.tsx
+++ src/api/employeeApi.tsx
@@ -1,7 +1,8 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
 import { Contract } from "./contractApi";
+import "./employee.css";
 
 const API_URL = "http://localhost:9000";
 
@@ -15,29 +16,52 @@ interface Employee {
     contracts?: Contract[];
 }
 
-const EmployeeList = () => {
+const EmployeeList: React.FC = () => {
     const [employees, setEmployees] = useState<Employee[]>([]);
 
     useEffect(() => {
-        axios.get(`${API_URL}/employees`).then((response) => {
+        axios.get<Employee[]>(`${API_URL}/employees`).then((response) => {
             setEmployees(response.data);
         });
     }, []);
+
+    const handleDelete = (id: number) => {
+        axios.delete(`${API_URL}/employees/${id}`).then(() => {
+            setEmployees(employees.filter((e) => e.id !== id));
+        });
+    };
+
     return (
         <div>
-            <h2> Employee List</h2>
-            <ul>
-                {" "}
-                {employees.map((employee) => (
-                    <li key={employee.id}>
-                        {employee.firstName} - {employee.lastName} -{" "}
-                        {employee.email} - {employee.address} -{" "}
-                        {employee.mobileNumber}
-                        {" | "}
-                        <Link to={`/contracts/${employee.id}`}>Contracts</Link>
-                    </li>
-                ))}
-            </ul>
+            <h2>Employee List</h2>
+
+            {employees.map((employee) => (
+                <div key={employee.id}>
+                    <strong>
+                        {employee.firstName} {employee.lastName}
+                    </strong>
+                    {employee.contracts && employee.contracts.length > 0 ? (
+                        <div>
+                            {employee.contracts.map((contract) => (
+                                <p key={contract.id}>{contract.contractType}</p>
+                            ))}
+                        </div>
+                    ) : (
+                        <p>No Contracts</p>
+                    )}
+                    {employee.email}
+                    <br />
+
+                    <Link to={`/contracts/${employee.id}`}>
+                        View All Contracts
+                    </Link>
+                    <br />
+                    <button onClick={() => handleDelete(employee.id)}>
+                        Delete
+                    </button>
+                    <hr />
+                </div>
+            ))}
         </div>
     );
 };
